a69d4f5403a2343cac64ff1775ab2ffa47397a30
Eduardo Rocha <eduardomfrocha@gmail.com>
2022-12-02 14:45:38 -0300

    Created SET service

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import ExplanationCard from "../../components/explanation/ExplanationCard";
 import DefaultButton from "../../components/common/DefaultButton";
-// import ChangeNavigationService from "../../service/ChangeNavigationService";
+import ChangeNavigationService from "../../services/ChangeNavigationService";
 
 export default function AppExplanation() {
   const navigation = useNavigation();
+  const [showHome, setShowHome] = useState("false");
+  const startDate = new Date();
+  const appStartData = `${startDate.getFullYear()}-${startDate.getMonth()}-${startDate.getDate()}`;
 
   function handleNavHome() {
     navigation.navigate("Home");
   }
 
+  function handleSetShowHome() {
+    if (showHome !== "true") {
+      ChangeNavigationService.setShowHome({ showHome: "true", appStartData })
+        .then(() => console.log(`Sucesso! ${showHome} ${appStartData}`))
+        .catch((err) => console.log(err));
+      setShowHome("true");
+      handleNavHome();
+    }
+    handleNavHome();
+  }
   return (
     <View style={styles.container}>
       <ScrollView>
@@ -30,7 +43,7 @@ export default function AppExplanation() {
           </Text>
           <DefaultButton
             buttonText={"Continuar"}
-            handlePress={handleNavHome}
+            handlePress={handleSetShowHome}
             width={250}
             height={50}
           />
