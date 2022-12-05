e4fe595d1e36c0595b8228855ddd4f0b0cbe9a4c
Eduardo Rocha <eduardomfrocha@gmail.com>
2022-12-05 14:08:09 -0300

    Linked page with read habit service

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import HabitsService from "../../services/HabitsService";
+
 import { useNavigation } from "@react-navigation/native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 import LifeStatus from "../../components/common/LifeStatus";
 import StatusBar from "../../components/Home/StatusBar";
@@ -10,7 +12,8 @@ import ChangeNavigationService from "../../services/ChangeNavigationService";
 
 export default function ({ route }) {
   const navigation = useNavigation();
-  const [mindHabit, setHabit] = useState();
+  // const [mindHabit, setHabit] = useState();
+  const [mindHabit, setMindHabit] = useState();
   const [moneyHabit, setMoneyHabit] = useState();
   const [bodyHabit, setBodyHabit] = useState();
   const [funHabit, setFunHabit] = useState();
@@ -22,6 +25,19 @@ export default function ({ route }) {
   };
   console.log("robotDaysLife1", robotDaysLife);
   useEffect(() => {
+    HabitsService.findByArea("Mente").then((mind) => {
+      setMindHabit(mind[0]);
+    });
+    HabitsService.findByArea("Financeiro").then((money) => {
+      setMoneyHabit(money[0]);
+    });
+    HabitsService.findByArea("Corpo").then((body) => {
+      setBodyHabit(body[0]);
+    });
+    HabitsService.findByArea("Humor").then((fun) => {
+      setFunHabit(fun[0]);
+    });
+
     ChangeNavigationService.checkShowHome(1)
     .then((showHome) => {
       const formDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
@@ -40,46 +56,23 @@ export default function ({ route }) {
           <StatusBar />
 
           {mindHabit ? (
-            <EditHabit
-              habit={mindHabit?.habitName}
-              frequency={`${mindHabit?.habitTime} - ${mindHabit?.habitFrequency}`}
-              habitArea={mindHabit?.habitArea}
-              checkColor="#90B7F3"
-            />
+            <EditHabit habit={mindHabit} checkColor="#90B7F3" />
           ) : (
             <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
           )}
-
           {moneyHabit ? (
-            <EditHabit
-              habit={moneyHabit?.habitName}
-              frequency={`${moneyHabit?.habitTime} - ${moneyHabit?.habitFrequency}`}
-              habitArea={moneyHabit?.habitArea}
-              checkColor="#85BB65"
-            />
+            <EditHabit habit={moneyHabit} checkColor="#85BB65" />
           ) : (
             <CreateHabit habitArea="Financeiro" borderColor="#85BB65" />
           )}
-
           {bodyHabit ? (
-            <EditHabit
-              habit={bodyHabit?.habitName}
-              frequency={`${bodyHabit?.habitTime} - ${bodyHabit?.habitFrequency}`}
-              habitArea={bodyHabit?.habitArea}
-              checkColor="#FF0044"
-            />
+            <EditHabit habit={bodyHabit} checkColor="#FF0044" />
           ) : (
             <CreateHabit habitArea="Corpo" borderColor="#FF0044" />
           )}
-
           {funHabit ? (
-            <EditHabit
-              habit={funHabit?.habitName}
-              frequency={`${funHabit?.habitTime} - ${funHabit?.habitFrequency}`}
-              habitArea={funHabit?.habitArea}
-              checkColor="#FE7F23"
-            />
-          ) : (
+            <EditHabit habit={funHabit} checkColor="#FE7F23" />
+            ) : (
             <CreateHabit habitArea="Humor" borderColor="#FE7F23" />
           )}
         </View>
